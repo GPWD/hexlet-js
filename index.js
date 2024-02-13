42ace8ef7b1f0f5688ce43e99866149d7d3e5931
georgy <palatckingeogy@yandex.ru>
2024-02-13 12:33:26 +0300

    Bug fix

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import app from './src/index.js';
 import _ from 'lodash';
 
-app();
-console.log(_.last(['one', 'two']));
-console.log('Hello, Hexlet!');
+// app();
+// console.log(_.last(['one', 'two']));
+// console.log('Hello, Hexlet!');
 
-
+console.log(_.without([1, 3, 4], 'two'));
